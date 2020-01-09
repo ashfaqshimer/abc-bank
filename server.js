18742cc00eda220fcadf8bd7193af4534960edee
Ashfaq Shimer <shimer.ahamed@gmail.com>
2020-01-09 13:29:12 +0530

    Remove unused code and dependencies

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Core dependencies
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 // Security libraries
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Local imports
 const connectDb = require('./config/db');
@@ -15,3 +18,43 @@ dotenv.config({ path: './config/config.env' });
 
 // Connect to database
 connectDb();
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Mount routers
+
+// Error handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.error(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
