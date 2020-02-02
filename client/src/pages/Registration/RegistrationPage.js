ce7623d399cb78fedd3471a5782a67b2c6288367
ashfaqshimer <shimer.ahamed@gmail.com>
2020-02-02 12:57:44 +0530

    Bind values with state in registration form

--- client/src/pages/Registration/RegistrationPage.js
+++ client/src/pages/Registration/RegistrationPage.js
@@ -5,22 +5,29 @@ import Container from 'react-bootstrap/Container';
 
 export default class RegistrationPage extends Component {
 	state = {
-		email: ''
+		email: '',
+		name: '',
+		address: '',
+		number: '',
+		nic: ''
 	};
 
 	handleChange = (evt) => {
-		this.setState({ [evt.target]: evt.target.value });
+		this.setState({ [evt.target.name]: evt.target.value });
 	};
 
 	render() {
 		return (
 			<Container>
+				<h1 className='display-3 text-center'>Register with us</h1>
+				<hr className='my-5' />
 				<Form>
 					<Form.Group controlId='formBasicEmail'>
 						<Form.Label>Email address</Form.Label>
 						<Form.Control
 							type='email'
 							placeholder='Enter email'
+							name='email'
 							value={this.state.email}
 							onChange={this.handleChange}
 						/>
@@ -30,13 +37,48 @@ export default class RegistrationPage extends Component {
 						</Form.Text>
 					</Form.Group>
 
-					<Form.Group controlId='formBasicPassword'>
-						<Form.Label>Password</Form.Label>
-						<Form.Control type='password' placeholder='Password' />
+					<Form.Group controlId='formBasicName'>
+						<Form.Label>Name</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Name'
+							name='name'
+							value={this.state.name}
+							onChange={this.handleChange}
+						/>
 					</Form.Group>
-					<Form.Group controlId='formBasicCheckbox'>
-						<Form.Check type='checkbox' label='Check me out' />
+
+					<Form.Group controlId='formBasicAddress'>
+						<Form.Label>Address</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Address'
+							name='address'
+							value={this.state.address}
+							onChange={this.handleChange}
+						/>
 					</Form.Group>
+					<Form.Group controlId='formBasicContact'>
+						<Form.Label>Contact Number</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='Contact Number'
+							name='number'
+							value={this.state.number}
+							onChange={this.handleChange}
+						/>
+					</Form.Group>
+					<Form.Group controlId='formBasicNIC'>
+						<Form.Label>NIC Number</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder='NIC Number'
+							name='nic'
+							value={this.state.nic}
+							onChange={this.handleChange}
+						/>
+					</Form.Group>
+
 					<Button variant='primary' type='submit'>
 						Submit
 					</Button>
